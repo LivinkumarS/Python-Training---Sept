ea7bf25a27054ef0969dee45b55552bec2ad9de8
LivinkumarS <001liv100@ggmail.com>
2025-09-04 10:50:30 +0530

    Introduction

---
+++ Introduction.py
@@ -0,0 +1,23 @@
+a=10
+b='Hello world'
+c=2.20
+d=True 
+
+# print(a)
+print(a)
+
+# This below line is used to promt data from user.
+
+'''Multiple Line Comment
+Multiple Line Comment
+Multiple Line Comment
+Multiple Line Comment
+Multiple Line Comment
+Multiple Line Comment
+Multiple Line Comment
+Multiple Line Comment
+Multiple Line Comment'''
+
+# a1=input("Enter the text: ")
+
+# print(a1)
