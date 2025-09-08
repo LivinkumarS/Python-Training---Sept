2bb9cb759e897e6901949d9741fe19562c2842b7
LivinkumarS <001liv100@ggmail.com>
2025-09-08 11:07:08 +0530

    Variables, Datatype and type casting

---
+++ VariablesDatatypeTypecasting.py
@@ -0,0 +1,34 @@
+# apple=1
+# _apple=2
+# Ball=3 
+
+# 1a=-7
+
+
+# apple='Apple. It is a fruit!'
+
+# print(Apple)
+# print=2
+
+# print(1+4)
+
+# str1="Hello"
+# str2="World!"
+# final=str1+str2
+# print(final)
+
+name="Surya"
+age=52
+
+# print(type(age))
+# print(type(name))
+# print(type(True))
+
+# print("His name is "+name+". He is "+str(age)+" years old.")
+# print(f'His name is {name}. He is {age} years old!')
+
+# His name is Vijay. He is 53 years old!
+
+# number1=int("the") #123
+
+# print(type(number1))
