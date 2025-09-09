b9453c5cc729310a7f3ff4c9cef17c45a556dd06
LivinkumarS <001liv100@ggmail.com>
2025-09-09 11:23:42 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,50 @@
+
+# print(a+b)
+# print("hello"+"world")
+
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+# print(a//b)
+
+# print(a%b)
+
+# print(a**b)
+
+# print(2+4-6*9/3-1)
+
+# a=10
+# b=20
+
+# print(a==b)
+# print(a<b)
+# print(a>b) 
+# print(a<=b) # a<b or a==b
+# print(a>=b) # a>b or a==b
+
+# a=False
+# b=False
+
+# print(a and b)
+# print(a or b)
+
+# print(not (a and b))
+
+# a=10
+# a+=10 #a=a+10
+# # a-=20 #a=a-20
+# a*=100 # a=a*100 2000
+# a/=500 #4
+# a//=3 #a=a//3. 4//3. 1
+# a%=2 # a=a%2. 1
+# a**=400 #a=a power 400
+
+# print(a)
+
+# print(17 & 20)
+# print(17 | 20)
+# print(15<<2)
+# print(15>>2)
+
+print(~12) #-8
