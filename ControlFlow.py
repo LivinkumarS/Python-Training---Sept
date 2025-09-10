20702e2d8b9cb9012eeb7a30c3a1e60b326d111a
LivinkumarS <001liv100@ggmail.com>
2025-09-10 11:10:17 +0530

    Control Statement

---
+++ ControlFlow.py
@@ -0,0 +1,57 @@
+# print("Hello")
+# print("World")
+# print("!")
+
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("You are eligible to vote.")
+# else:
+#     print("You are not eligible to vote.")
+
+# a=23
+# b=-23
+
+# if a<b and a>=3: #false
+#     print("From If Block!")
+# elif a>b and a==b: #false
+#     print("From Elif1 Block!")
+# elif True: #true
+#     print("From Elif2 block")
+# elif False:
+#     print("From Elif3 Block")
+# else:
+#     print("From Else Block!")
+
+# print("Final Step")
+
+# a=int(input("marks: "))
+# if a>=90:
+#    print("O")
+# elif a>=80:
+#    print("A") 
+# elif a>=70:
+#    print("B")
+# elif a>=60:
+#    print("C")
+# elif a>=50:
+#    print("D")
+# else:
+#     print("Fail")
+
+# if True:
+#     if True:
+#         print("Hello World!")
+
+a=int(input("Enter a number: "))
+
+if a>10:
+    if a>15:
+        print("Very large")
+    else:
+        print("Large")
+else:
+    if a<5:
+        print("Very Small")
+    else:
+        print('Small')
+        
