0e307c527324ffc0455c494f64db214f38412e8b
LivinkumarS <001liv100@ggmail.com>
2025-09-11 11:14:16 +0530

    Loops

---
+++ Loops.py
@@ -0,0 +1,31 @@
+# i=0
+
+# while i<5:
+#     print("Step1")
+#     i-=1
+
+
+# print("Finished!")
+
+
+# a="Hello world"
+# print(a[5])
+
+# for i in a:
+#     print(i)
+
+#iteration count: 1,2,3,4,5
+# i=0,1,2,3,4
+
+# for i in range(1,51,2):
+#     print(i)
+
+for i in range(1,10): #[1,2,3,4,5,6,7,8,9]
+    if(i%3==0):
+        continue
+    print(i)
+
+# iteration count: 1,2,3,4
+# i=1,2,3,4
+
+# out: 1,2,4,5,7,8
