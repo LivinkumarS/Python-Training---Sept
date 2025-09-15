73cac8b574311255dbbabd6103605d040d0e8af6
LivinkumarS <001liv100@ggmail.com>
2025-09-15 11:15:07 +0530

    Datastructures-List,Set,Tuple

---
+++ Datastructures.py
@@ -0,0 +1,57 @@
+# str1="Hello world"
+
+# arr1[4]="seven"
+
+# print(arr1[2]+arr1[1])
+# print(arr1[5:])
+# print(arr1[2:5])
+# print(str1[5])
+
+# print(str1[5:])
+
+arr1=[1,2,3,"four","five","six",True, False, 5.55,6.77,3,4,5,6]
+
+# arr1.append(99)
+# arr1.append(199)
+
+# arr1.insert(7, "Added String")
+
+# arr1.remove(5.55)
+
+# arr1.remove(3)
+
+# arr1.pop(3)
+
+# arr2=[4,432,53,32,65,6,45,23,4,6,789]
+
+# arr2.sort()
+# arr2.reverse()
+
+# print(arr2.count(650))
+# print(arr2.index(400))
+
+
+
+# list1=[1,2,3,4,5,[6,7,8,9,10,["eleven","twelve"]]]
+
+# print(list1[-1][-1][-1][-1])
+
+# tup1=(11,12,13,14,14,13,12,11)
+
+# tup1[3]=100
+
+# print(tup1)
+
+# a=('livin',22,'fed')
+# name,age,role=a
+
+# print(name)
+# print(age)
+# print(role)
+
+set1={2,2,3,4,5,6,7,7,8,5,45}
+set2={7,8,9,10,4,5,45,54}
+
+# print(set1.intersection(set2))
+print(set1.difference(set2))
+# print(set1.union(set2))
