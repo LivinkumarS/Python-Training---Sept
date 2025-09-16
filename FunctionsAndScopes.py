bd06d3c98a46e25ac62c1f4c4f40e008a40f4f9e
LivinkumarS <001liv100@ggmail.com>
2025-09-16 11:18:46 +0530

    Functions and Scopes

---
+++ FunctionsAndScopes.py
@@ -0,0 +1,50 @@
+    
+    
+
+    
+# greet()
+# greet()
+# greet()
+
+# def computeValue(a,b,c): 
+#     print(a*b*c)
+
+# computeValue(2,4,6)
+# computeValue(b=3,c=9,a=1)
+
+def computeValue(a=10): #a=20
+    print(a**2)
+
+# computeValue(20)
+# computeValue()
+
+def greet():
+    print("Good Morning!")
+    print("Good Afternoon!!")
+    print("Good Evening!!!")
+    return 397
+    print("Good Night!!!!")
+    print("--------------------------------------")
+    
+# a=greet()
+# print(greet()+3)
+
+def calcNum(num1,num2):
+    return num1*num2 
+
+# print(calcNum(2,calcNum(3,calcNum(4,calcNum((1/3),3)))))
+
+# a=10
+
+def printName():
+    a=10
+    print(f"The a's value from inside the function1: {a}")
+
+def printName1():
+    print(f"The a's value from inside the function1: {a}")
+
+    
+
+printName()    
+printName1()    
+print(f"The a's value from outside the function1: {a}")
