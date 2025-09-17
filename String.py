5a349fba2301e27764e2f76b480f9974b78bf26d
LivinkumarS <001liv100@ggmail.com>
2025-09-17 11:00:03 +0530

    Strings

---
+++ String.py
@@ -0,0 +1,39 @@
+
+str3='''In 2013, Kohli was ranked number one in the ICC rankings for ODI batsmen.
+In 2015, he achieved the summit of T20I rankings.
+In 2020, the International Cricket Council named him the male cricketer of the decade.'''
+
+# str3[4]='7'
+
+# ans=str1[2:8]
+# ans=str1*5
+
+# ans="love to" not in str1
+
+# ans=str1.upper()
+# ans=str1.lower()
+# ans=str1.title()
+
+
+# ans=str1.isdigit()
+# ans=str1.isalpha()
+# ans="123!abc".isalnum()
+
+# ans=str2.find("the")
+# ans=str2.count("red")
+# ans=str2.replace("love","like")
+# ans=str2.strip()
+str1='    Iwouldlovetocode       '
+str2="I would love to learn!"
+
+print(len(str2))
+# ['I','would','love'...]
+
+lis1=['I', 'would', 'love', 'to', 'learn!']
+
+# ans=str2.split("l")
+
+ans="--".join(lis1)
+
+print(ans)
+# print(str2)
